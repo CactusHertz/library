393a7ba0e97aa09fc79913d874eeafea199d900f
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2024-02-08 21:48:58 -0800

    Add book constructor

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read){
+    // contructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+
+    this.info = function() {
+        return (this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read)
+    };
+}
+
+function AddBookToLibrary() {
+    // do stuff here
+}
