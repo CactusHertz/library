44a9cf1af212ed619ef405c1365a75762f419952
CactusHertz <50159128+CactusHertz@users.noreply.github.com>
2024-02-09 21:26:13 -0800

    Add function to add and display books

--- script.js
+++ script.js
@@ -13,5 +13,43 @@ function Book(title, author, pages, read){
 }
 
 function AddBookToLibrary() {
-    // do stuff here
+    var tempBook = new Book("test title", "test author", Math.floor(Math.random() * 100), false);
+    myLibrary.push(tempBook);
 }
+
+function DisplayBooks(books) {
+    const list = document.querySelector('.books');
+    for (var i = 0; i < books.length; i++){
+        console.log(books[i].info());
+        list.appendChild(CreateBookCard(books[i]));
+    }
+}
+
+function CreateBookCard(book){
+    const card = document.createElement('div');
+
+    const title = document.createElement('div');
+    title.textContent = book.title;
+    const author = document.createElement('div');
+    author.textContent = book.author;
+    const pages = document.createElement('div');
+    pages.textContent = book.pages;
+    const read = document.createElement('input');
+    read.setAttribute('type', 'checkbox');
+    read.setAttribute('checked', 'true');
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+
+    card.appendChild(title);
+    card.appendChild(author);
+    card.appendChild(pages);
+    card.appendChild(read);
+    card.appendChild(deleteButton);
+
+    return card;
+}
+
+for(var i = 0; i < 10; i++){
+    AddBookToLibrary();
+}
+DisplayBooks(myLibrary);
